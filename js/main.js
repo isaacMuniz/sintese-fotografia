4531dadaa27747e7a46b27f0e73496f370e886e8
Isaac Muniz <isaacmuniz@zohomail.com>
2020-05-12 09:22:42 -0300

    pequenos melhorametos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // animate smooth scroll
-$('#view-work').on('click', function() {
-    const images = $('#images').position().top;
+$("#view-work").on("click", function () {
+  const images = $("#images").position().top;
 
-    $('html, body').animate(
-        {
-            scrollTop: images
-        },
-        900
-    );
+  $("html, body").animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
 });
